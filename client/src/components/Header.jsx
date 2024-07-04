b8c7f7ac66767cdcbcff5fc7dfdac15e049b04d0
Lepha <lepharamchiary@gmail.com>
2024-07-04 12:17:03 +0530

    navbar UI complete also small device

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -19,15 +19,15 @@ function Header() {
               </div>
             </div>
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-              <div className="relative ml-3">
-                <div className="flex md:gap-5 lg:gap-7 p-2 md:px-12 lg:p-18 text-sm cursor-pointer items-center">
+              <div className="relative flex mr-2 md:mr-10 lg:mr-12">
+                <div className="hidden md:flex md:gap-5 lg:gap-7 p-2 md:px-12 lg:p-18 text-sm cursor-pointer items-center">
                   <a>Our Story</a>
                   <a>Membership</a>
                   <a>Write</a>
                   <button>Sign in</button>
-                  <div className="flex bg-black h-8 w-auto p-6 md:p-4 lg:p-5 rounded-full items-center justify-center font-">
-                    <button className="text-white">Get Started</button>
-                  </div>
+                </div>
+                <div className="flex bg-black h-8 w-auto p-3 md:p-4 lg:p-5 rounded-full items-center justify-center">
+                  <button className="text-white">Get Started</button>
                 </div>
               </div>
             </div>
