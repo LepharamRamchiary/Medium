b6b135adf72386f9c3bac58c59f8983a27939c08
Lepha <lepharamchiary@gmail.com>
2024-07-05 13:57:03 +0530

    .

--- client/src/components/OurStoryHeader.jsx
+++ client/src/components/OurStoryHeader.jsx
@@ -26,7 +26,7 @@ function Header() {
               </div>
             </div>
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-              <div className="relative flex md:mr-2 gap-3 md:gap-5 lg:gap-5 font-semibold md:mr-10 lg:mr-12 md:items-center">
+              <div className="relative flex gap-3 md:gap-5 lg:gap-5 font-semibold md:mr-10 lg:mr-12 md:items-center">
                 <div className="flex bg-black h-8 w-auto p-3 md:p-4 lg:p-5 rounded-full items-center justify-center hover:bg-gray-800 transition-all">
                   <button className="text-white">Sign in</button>
                 </div>
