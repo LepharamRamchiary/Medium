abc85ed04480d25bee73c12fe59180e666758257
Lepha <lepharamchiary@gmail.com>
2024-07-06 12:32:25 +0530

    member page partillay done

--- client/src/components/MemberShip.jsx
+++ client/src/components/MemberShip.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import { PiStarThin } from "react-icons/pi";
 
 function MemberShip() {
   return (
-    <div className='mt-96'>MemberShip</div>
-  )
+    <div className="h-full w-ful">
+      <div className="md:mt-20 mt-40 flex flex-wrap">
+        <div className="flex flex-col md:w-3/5 p-5 md:p-10 md:border-r border-gray-950 border-b">
+          <div className="md:my-16 my-10">
+            <div className="">
+              <h1 className="md:text-8xl text-5xl lg:text-8xl font-serif">
+                Support human stories
+              </h1>
+              <p className="md:my-9 my-4 md:text-xl font-semibold">
+                Become a member to read without limits or ads, fund great
+                writers, and join a global community of people who care about
+                high-quality storytelling.
+              </p>
+            </div>
+            <div className="flex gap-3">
+              <div className="flex bg-black h-8 w-auto p-3 md:p-4 lg:p-5 rounded-full items-center justify-center hover:bg-gray-800 transition-all">
+                <button className="text-white">Get started</button>
+              </div>
+              <div className="flex bg-transparent h-8 w-auto p-3 md:p-4 lg:p-5 rounded-full items-center border border-gray-600 justify-center hover:bg-slate-200 transition-all">
+                <button className="text-black">View plans</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="md:p-10 p-6 md:w-2/5 border-gray-950 border-b flex flex-col justify-end">
+          <div className="flex flex-col md:my-10 my-20">
+            <div className="flex my-2 items-center bg-yellow-300 w-44 text-sm p-2 justify-center gap-3 rounded-full">
+              <PiStarThin />
+              <button>Member-only story</button>
+            </div>
+            <h1 className="text-2xl font-semibold font-serif my-6">
+              Hello world ewfgewfiewhfieohw ewfihewofihewofih efhoewifhew9
+            </h1>
+            <div className="flex gap-3 md:mt-4 items-center">
+              <div className="bg-red-500 h-10 w-10 rounded-full flex justify-center items-center">
+                A
+              </div>
+              <div className="">
+                <p>name</p>
+                <p>how is he/she ?</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default MemberShip
+export default MemberShip;
