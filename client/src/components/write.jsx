36559caa7e53206e5203cd2ea32697120adc500b
Lepha <lepharamchiary@gmail.com>
2024-08-27 13:31:13 +0530

    write page complete for large schreen

--- client/src/components/write.jsx
+++ client/src/components/write.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { GoBell } from "react-icons/go";
 import { BsThreeDots } from "react-icons/bs";
 import avatarImage from "../assets/ang.jpeg";
 import { CiCirclePlus } from "react-icons/ci";
+import { IoCloseCircleOutline } from "react-icons/io5";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { CiImageOn } from "react-icons/ci";
+import { FaUnsplash } from "react-icons/fa6";
+import { PiYoutubeLogoThin } from "react-icons/pi";
+import { ImEmbed } from "react-icons/im";
+import { PiBracketsCurlyLight } from "react-icons/pi";
+import { AiOutlinePartition } from "react-icons/ai";
 
-function write() {
+function Write() {
+  const [value, setValue] = useState("");
+  const [title, setTitle] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="relative w-full">
       <nav className="fixed top-2 w-full z-50">
         <div className="mx-auto max-w-6xl px-2 sm:px-6 lg:px-8">
-          <div className="relative flex h-14 items-center justify-between">
+          <div className="relative flex h-14 items-center md:justify-between">
             <div className="flex flex-1 items-center sm:items-stretch sm:justify-start">
               <div className="sm:ml-6 flex sm:block md:flex md:gap-2">
                 <div className="flex space-x-4">
@@ -34,7 +51,7 @@ function write() {
                     <img
                       className="h-10 w-10 rounded-full p-1 hidden md:block"
                       src={avatarImage}
-                      alt="avatar image"
+                      alt="avatar"
                     />
                   </div>
                 </div>
@@ -43,11 +60,48 @@ function write() {
           </div>
         </div>
       </nav>
-      <div className="relative top-32 max-w-4xl mx-auto">
+      <div className="relative top-32 md:max-w-4xl md:mx-auto">
         <div className="">
-          <div className="flex items-center divide-x divide-gray-100 gap-3">
-            <CiCirclePlus className="text-4xl"/>
-            <h1>Title</h1>
+          <div className="flex flex-col gap-5 px-3 md:items-start">
+            <div className="flex items-center divide-x divide-gray-100 gap-3">
+              {isOpen ? (
+                <IoCloseCircleOutline
+                  className="md:text-4xl text-xl font-thin cursor-pointer"
+                  onClick={toggleMenu}
+                />
+              ) : (
+                <CiCirclePlus
+                  className="md:text-4xl text-xl font-thin cursor-pointer"
+                  onClick={toggleMenu}
+                />
+              )}
+              <input
+                type="text"
+                className="md:text-3xl text-sm font-bold p-3 rounded-md focus:outline-none"
+                placeholder="Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+              {isOpen && (
+                <div className="absolute ml-12 p-4 bg-white ">
+                  <div className="flex gap-2">
+                    <CiImageOn className="h-9 text-sm w-9 text-green-400 border border-green-400 rounded-full p-2" />
+                    <FaUnsplash className="h-9 w-9 text-sm text-green-400 border border-green-400 rounded-full p-2" />
+                    <PiYoutubeLogoThin className="h-9 w-9 text-sm text-green-400 border border-green-400 rounded-full p-2" />
+                    <ImEmbed className="h-9 w-9 text-sm text-green-400 border border-green-400 rounded-full p-2" />
+                    <PiBracketsCurlyLight className="h-9 w-9 text-sm text-green-400 border border-green-400 rounded-full p-2" />
+                    <AiOutlinePartition className="h-9 w-9 text-sm text-green-400 border border-green-400 rounded-full p-2" />
+                  </div>
+                </div>
+              )}
+            </div>
+            <ReactQuill
+              theme="snow"
+              placeholder="Tell your story..."
+              value={value}
+              onChange={setValue}
+              className="md:w-full md:h-96 h-44"
+            />
           </div>
         </div>
       </div>
@@ -55,4 +109,4 @@ function write() {
   );
 }
 
-export default write;
+export default Write;
