43d66e10095834e70a3fc624378a18a1313893fd
Lepha <lepharamchiary@gmail.com>
2024-09-10 15:14:46 +0530

    backend setup complete

--- api/src/index.js
+++ api/src/index.js
@@ -1,9 +1,18 @@
-import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import {app} from './app.js'
+dotenv.config({
+    path: './.env'
+})
 
-const app = express()
 
-const port = 3000
 
-app.listen(port, () => {
-    console.log(`Server is live on ${port}`);
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
 })
